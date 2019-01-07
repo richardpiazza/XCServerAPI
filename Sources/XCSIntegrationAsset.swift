907e2367a341a066eb309f69e53e97013323ba34
Richard Piazza <richard@richardpiazza.com>
2019-01-06 18:21:59 -0600

    Added 'isDirectory' to integration asset.

--- Sources/XCSIntegrationAsset.swift
+++ Sources/XCSIntegrationAsset.swift
@@ -6,4 +6,5 @@ public struct XCSIntegrationAsset: Codable {
     public var allowAnonymousAccess: Bool?
     public var relativePath: String?
     public var triggerName: String?
+    public var isDirectory: Bool?
 }
